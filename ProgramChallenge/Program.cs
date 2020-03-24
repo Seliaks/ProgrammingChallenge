5a3e4ab816506ff7d0abaa6bc7f384b9cbfb30c5
Robin Skailes <skailes.ri@icloud.com>
2020-03-24 12:54:53 +0000

    Initial Commit

---
+++ ProgramChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramChallenge
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
