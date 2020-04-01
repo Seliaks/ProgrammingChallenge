3e84fefc7219b265eb5d048261a4efdb8529a88c
Robin Skailes <skailes.ri@icloud.com>
2020-04-01 20:16:58 +0100

    Added Main Menu

--- ProgramChallenge/Program.cs
+++ ProgramChallenge/Program.cs
@@ -7,6 +7,22 @@ namespace ProgramChallenge
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        public static int Menu()
+        {
+            Console.Write(@"1: Queue
+2: List
+3: Stack
+4: Hash Table
+5: Dictionary2983woIE
+6: Graphs
+7: Trees
+8: Vectors
+9: Quit
+Enter choice: ");
+            return int.Parse(Console.ReadLine());
         }
     }
 }
---
+++ ProgramChallenge/Queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace ProgramChallenge
+{
+    public class Queue
+    {
+        private int size = 0;
+        private int frontP = 0;
+        private int rearP = -1;
+        private int maxSize;
+        private int[] MyQueue;
+
+        public Queue(int maxSize)
+        {
+            this.maxSize = maxSize;
+            MyQueue = new int[maxSize];
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        public bool IsFull()
+        {
+            return size == maxSize;
+        }
+
+        public virtual void Enqueue(int newItem)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue full");
+            }
+            else
+            {
+                rearP++;
+                MyQueue[rearP] = newItem;
+                size++;
+            }
+        }
+
+        public virtual int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue empty");
+                return 0;
+            }
+            else
+            {
+                int output = MyQueue[frontP];
+                frontP++;
+                size--;
+                return output;
+            }
+        }
+    }
+}
